1008bd2101a6a1bda106393d9dfe662fc3a3d796
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-06-23 14:41:47 -0400

    progress

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,19 +1,18 @@
 export const CHUNK_SIZE = 64 as const
-
 export const GRAVITY: number = 1
 export const PLAYER_JUMP_POWER = -7
 export const PLAYER_MOVE_SPEED = 3.5
-
-export const MAX_MATTER_PARTICLES = 1500
-export const VFX_PARTICLE_TO_TERRAIN_CHUNK_SIZE = 4
-
 export const DEFAULT_PLAYER_STARTING_MATTER = 400
-
 export const DEFAULT_PLAYER_MATTER_TANK_SIZE = 5000
 
-export const GLOW_ENABLED = true
-
+// vfx limits
+export const MAX_MATTER_PARTICLES = 1500
+export const VFX_PARTICLE_TO_TERRAIN_CHUNK_SIZE = 4
 export const MAX_PROJECTILES = 500
+
+// shaders
+export const GLOW_ENABLED = true
+export const ICE_TEXTURE_ENABLED = true
 
 // debug
 export const ENABLE_PANE_DEBUG = true
